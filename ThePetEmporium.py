1924b26968038d35e978500dee9991e536ea35ae
Aaron Joyce <aronwanderer@gmail.com>
2016-01-24 16:22:59 -0600

    Added in some random web information for deployment to the server.  Ignore it

--- ThePetEmporium.py
+++ ThePetEmporium.py
@@ -1,5 +1,26 @@
 """The Pet Emporium, A programming collaboration between Aaron and Leah"""
 
+from flask import Flask, url_for
+app = Flask(__name__)
+
+def from_py(hello):
+  def wrapper(*args, **kwargs):
+    return hello(*args, **kwargs) + " From Python!"
+  return wrapper
+
+@app.route('/login')
+def login():
+    return "You Logged In!"
+    
+@app.route("/")
+@from_py
+def hello():
+    return "Hello World! " + "<a href=" + url_for('login') + "> Login </a><br>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+
 print('Welcome to The Pet Emporium!')
 
 
