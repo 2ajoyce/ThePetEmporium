88348ed4fa2276c7d8523bd3c264b2d473a450f6
2ajoyce <aronwanderer@gmail.com>
2016-01-24 08:32:05 -0600

    Added pypet.py

---
+++ pypet.py
@@ -0,0 +1,36 @@
+print 'Welcome to Pypet!'
+
+
+cat = {
+  'name' : 'Fluffy',
+  'hungry' : True,
+  'weight' : 9.5,
+  'age' : 5,
+  'photo' : '(=^o.o^=)__',
+}
+
+mouse = {
+  'name' : 'Nibbles',
+  'age' : 6,
+  'weight' : 1.5,
+  'hungry' : True,
+  'photo' : '<:3 )~~~~',
+}
+
+pets = [cat, mouse]
+
+print 'Hello ' + cat['name']
+print cat['photo']
+print 'Hello ' + mouse['name']
+print mouse['photo']
+
+def feed(pet):
+  if pet['hungry'] == True:
+    pet['hungry'] = False
+    pet['weight'] =  pet['weight'] + 1
+  else:
+     print 'The Pypet is not hungry!'
+
+for bed in pets:
+  feed(bed)
+  print bed
