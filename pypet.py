68d5819a81f2d8f08d597be9a2bee633c0523562
2ajoyce <aronwanderer@gmail.com>
2016-01-24 11:18:34 -0600

    Made changes to fit file to PEP-8 standards using pylint

--- pypet.py
+++ pypet.py
@@ -1,37 +1,39 @@
-#Aaron and Leah's Project
-print 'Welcome to Pypet!'
+"""The Pet Emporium, A programming collaboration between Aaron and Leah"""
+
+print('Welcome to Pypet!')
 
 
 cat = {
-  'name' : 'Fluffy',
-  'hungry' : True,
-  'weight' : 9.5,
-  'age' : 5,
-  'photo' : '(=^o.o^=)__',
+    'name' : 'Fluffy',
+    'hungry' : True,
+    'weight' : 9.5,
+    'age' : 5,
+    'photo' : '(=^o.o^=)__',
 }
 
 mouse = {
-  'name' : 'Nibbles',
-  'age' : 6,
-  'weight' : 1.5,
-  'hungry' : True,
-  'photo' : '<:3 )~~~~',
+    'name' : 'Nibbles',
+    'age' : 6,
+    'weight' : 1.5,
+    'hungry' : True,
+    'photo' : '<:3 )~~~~',
 }
 
 pets = [cat, mouse]
 
-print 'Hello ' + cat['name']
-print cat['photo']
-print 'Hello ' + mouse['name']
-print mouse['photo']
+print('Hello ' + cat['name'])
+print(cat['photo'])
+print('Hello ' + mouse['name'])
+print(mouse['photo'])
 
 def feed(pet):
-  if pet['hungry'] == True:
-    pet['hungry'] = False
-    pet['weight'] =  pet['weight'] + 1
-  else:
-     print 'The Pypet is not hungry!'
+    """Feeds the pet that is passed into it.  The pet will stop being hungry and gain weight."""
+    if pet['hungry']:
+        pet['hungry'] = False
+        pet['weight'] = pet['weight'] + 1
+    else:
+        print('The Pypet is not hungry!')
 
 for bed in pets:
-  feed(bed)
-  print bed
+    feed(bed)
+    print(bed)
