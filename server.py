15bdff9f76dc71d188f39685dee46b601006d13a
Aaron Joyce <aronwanderer@gmail.com>
2016-01-24 21:59:19 -0600

    Trying to import ThePetEmporium into server.py

--- server.py
+++ server.py
@@ -4,8 +4,8 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from ThePetEmporium.py import ThePetEmporium
+from ThePetEmporium import hello
 
-http_server = HTTPServer(WSGIContainer(app))
+http_server = HTTPServer(WSGIContainer(hello))
 http_server.listen(5000)
 IOLoop.instance().start()
