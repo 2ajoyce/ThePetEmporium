eca75218f294e15aabb47011fb1c3a5ffb860d1c
Aaron Joyce <aronwanderer@gmail.com>
2016-01-24 22:02:40 -0600

    Managed to import file, but things are working strangly when I attempt to serve it

--- server.py
+++ server.py
@@ -4,8 +4,8 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from ThePetEmporium import hello
+from ThePetEmporium import login
 
-http_server = HTTPServer(WSGIContainer(hello))
+http_server = HTTPServer(WSGIContainer(login))
 http_server.listen(5000)
 IOLoop.instance().start()
