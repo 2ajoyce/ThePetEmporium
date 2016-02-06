22b62f4e1cfb7c79886c1f13259638b2d650d355
root <root@localhost.localdomain>
2016-02-05 20:00:29 -0600

    rearranged pets

--- makeit.py
+++ makeit.py
@@ -2,21 +2,6 @@
 
 import json
 
-def feed(pet):
-    """Feeds the pet that is passed into it.  The pet will stop being hungry and gain weight."""
-    if pet['hungry']:
-        pet['hungry'] = False
-        pet['weight'] = pet['weight'] + 1
-    else:
-        print('Your pet is not hungry!')
-
-def importPets():
-    pets = json.loads(open('pets.json').read())
-    return pets
-
-# Greet user and ask what pet they would like to interact with
-# Show list of pets with phots and names
-def main():
     cat = {
         'name' : 'Triangle',
         'hungry' : True,
@@ -32,21 +17,34 @@ def main():
         'hungry' : True,
         'photo' : '<:3 )~~~~',
     }
+
+def feed(pet):
+    """Feeds the pet that is passed into it.  The pet will stop being hungry and gain weight."""
+    if pet['hungry']:
+        pet['hungry'] = False
+        pet['weight'] = pet['weight'] + 1
+    else:
+        print('Your pet is not hungry!')
+
+def importPets():
+    pets = json.loads(open('pets.json').read())
+    return pets
+
+def pickaPet():
+
+    return pet
+
+# Greet user and ask what pet they would like to interact with
+# Show list of pets with phots and names
+def main():
     print('Welcome to The Pet Emporium!')
     print('Who would you like to play with?')
-    print(cat['photo'])
-    print(mouse['photo'])
+    print(cat['name'], cat['photo'])
+    print(mouse['name'], mouse['photo'])
 
-    kennel = importPets()
-    print(kennel)
-    
-
+    kennel = importPets()    
+    pet = pickaPet()
     pets = [cat, mouse]
-
-    print('Hello ' + cat['name'])
-    print(cat['photo'])
-    print('Hello ' + mouse['name'])
-    print(mouse['photo'])
 
     for animal in pets:
         feed(animal)
