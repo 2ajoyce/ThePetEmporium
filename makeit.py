d604c699810ef8020918f5abc19f7172faa6e421
Aaron Joyce <aronwanderer@gmail.com>
2016-02-05 20:44:48 -0600

    Changed pet to a class Added feed as a memeber function of Pet Fixed import pet function

--- makeit.py
+++ makeit.py
@@ -18,17 +18,39 @@ mouse = {
         'photo' : '<:3 )~~~~',
     }
 
-def feed(pet):
-    """Feeds the pet that is passed into it.  The pet will stop being hungry and gain weight."""
-    if pet['hungry']:
-        pet['hungry'] = False
-        pet['weight'] = pet['weight'] + 1
-    else:
-        print('Your pet is not hungry!')
+class Pet:
+    """A Pet Class"""
+    def __init__(self, type, name, age, weight, hungry, photo):
+        self.type = type
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.hungry = hungry
+        self.photo = photo
+
+    def feed(self):
+        """Feeds the pet. The pet will stop being hungry and gain weight."""
+        if self.hungry:
+            self.hungry = False
+            self.weight += 1
+        else:
+            print('Your pet is not hungry!')
 
 def importPets():
     pets = json.loads(open('pets.json').read())
-    return pets
+    Kennel = []
+
+    for index, pet in pets.items():
+        pType = pet[0]['type']
+        pName = pet[1]['name']
+        pAge = pet[2]['age']
+        pWeight = pet[3]['weight']
+        pHungry = pet[4]['hungry']
+        pPhoto = pet[5]['photo']
+        Kennel.append(Pet(pType, pName, pAge, pWeight, pHungry, pPhoto))
+        print(Kennel)
+    return Kennel
+
 
 # Lets user pick a pet from the list shown to interact with
 def pickaPet():
