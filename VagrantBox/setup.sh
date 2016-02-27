79a3b6b105cb044313ad4308f523b7950894f223
Aaron Joyce <aronwanderer@gmail.com>
2016-02-26 22:54:24 -0600

    Moved Vagrant Box to new repository

--- VagrantBox/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update
-apt-get install -y apache2 python-pip git
-git clone https://github.com/2ajoyce/ThePetEmporium.git
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
-fi
